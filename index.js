82a665ee553fb61d8ebee30168d3cb2c368bd2c5
Tawhid-01 <mtworld008@gmail.com>
2025-02-08 23:48:41 +0600

    simple game

---
+++ index.js
@@ -0,0 +1,12 @@
+//simple game in js
+
+let gameNum=25;
+
+let user = prompt("guss the number :");
+
+
+while(user != gameNum){
+  user =prompt("you enter the wrong number.try again :")
+
+}
+console.log("congratulation, you enter the right number :");
